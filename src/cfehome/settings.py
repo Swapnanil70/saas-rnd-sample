23faedcd2eb81a51427595eadfece89970de6138
Swapnanil Goswami <swapnanil-mac@Swapnanils-MacBook-Air.local>
2025-05-21 22:57:16 +0530

    Deployment : Ready for Railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-2iw1p+%t!&uu)4=a%8bf)mh(c2igpv1ha%*nr+r-p7$f&i#k&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app", # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += ["localhost", "127.0.0.1"]
 
 
 # Application definition
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -6,9 +6,30 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+
     my_title = "Home Page"
-    my_context = {"title": my_title}
+    my_context = {"title": my_title,
+                  "page_views": page_qs.count(),
+                  "percent": 100 if qs.count() == 0 else round(page_qs.count() * 100 / qs.count(), 2),
+                  "total_views": qs.count()}
+    html_template = "home.html"
+
+    PageVisit.objects.create(path=request.path)
+
+    return render(request, html_template, my_context)
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+
+    my_title = "About Page"
+    my_context = {"title": my_title,
+                  "page_views": page_qs.count(),
+                  "percent": 100 if qs.count() == 0 else round(page_qs.count() * 100 / qs.count(), 2),
+                  "total_views": qs.count()}
     html_template = "home.html"
 
     PageVisit.objects.create(path=request.path)
